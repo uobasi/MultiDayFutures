555c7687294d3aabcbc6d6018cbafc7416663ac1
uobasi <54387747+uobasi@users.noreply.github.com>
2024-08-20 17:46:03 -0400

    .

--- app.py
+++ app.py
@@ -1213,7 +1213,8 @@ def update_graph_live(n_intervals, data):
     fig.add_trace(go.Scatter(x=pd.Series([i for i in range(len(df))]), y=df['STDEV_025Cum'], mode='lines', opacity=0.5, name='UPPERVWAP0.25', line=dict(color='black')))
     fig.add_trace(go.Scatter(x=pd.Series([i for i in range(len(df))]), y=df['STDEV_N025Cum'], mode='lines', opacity=0.5, name='LOWERVWAP0.25', line=dict(color='black')))
            
-
+    fig.add_trace(go.Scatter(x=pd.Series([i for i in range(len(df))]), y=df['prevDayPOC'], mode='lines', opacity=0.5, name='prevDayPOC', line=dict(color='cyan')))
+       
 
 
     fig.add_trace(go.Scatter(x=pd.Series([i for i in range(len(df))]), y=df['vwapCum'], mode='lines', name='vwapCum', line=dict(color='crimson')))
