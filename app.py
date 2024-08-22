8da7e60422d17d375fdac2728ffc45206eaf31eb
uobasi <54387747+uobasi@users.noreply.github.com>
2024-08-22 18:47:01 -0400

    .

--- app.py
+++ app.py
@@ -1146,8 +1146,8 @@ def update_graph_live(n_intervals, data):
                 else 'gray' for i in df['buySellDif']] #tst
     
     #fig.add_trace(go.Bar(x=pd.Series([i for i in range(len(df))]), y=df['buySellDif'], marker_color=coll), row=2, col=1) #tst
-    fig.add_trace(go.Scatter(x=pd.Series([i for i in range(len(df))]), y=df['topOrderBuyPercent'], marker_color=green), row=2, col=1) #tst
-    fig.add_trace(go.Scatter(x=pd.Series([i for i in range(len(df))]), y=df['topOrderSellPercent'], marker_color=red), row=2, col=1)
+    fig.add_trace(go.Scatter(x=pd.Series([i for i in range(len(df))]), y=df['topOrderBuyPercent'], marker_color='green'), row=2, col=1) #tst
+    fig.add_trace(go.Scatter(x=pd.Series([i for i in range(len(df))]), y=df['topOrderSellPercent'], marker_color='red'), row=2, col=1)
     '''
     fig.add_trace(
         go.Bar(
