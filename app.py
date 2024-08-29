ccda96e0df4d30c288452ba094bb9398202ac255
uobasi <54387747+uobasi@users.noreply.github.com>
2024-08-29 03:29:40 -0400

    .

--- app.py
+++ app.py
@@ -1050,12 +1050,35 @@ def update_graph_live(n_intervals, sname, stored_data, interval_time, previous_s
         df1 = pd.DataFrame(finall, columns=['topOrderBuy', 'topOrderSell', 'topOrderBuyPercent', 'topOrderSellPercent'])
         df = pd.concat([df, df1],  axis = 1)
         
+        temp = pd.concat([stored_data, df], ignore_index=True)
+        for trt in tpo:
+            for t in trt:
+                t[6] = temp['timestamp'].searchsorted(t[2])
+                
+        buys = 0
+        sells = 0
+        finall = []
+        for i in range(len(tpo)):
+            for x in tpo[i]:
+                if x[6] == len(stored_data)+i:
+                    if x[3] == 'A':
+                        sells+=x[1]
+                    elif x[3] == 'B':
+                        buys+=x[1]
+            finall.append([buys,sells])
+            buys = 0
+            sells = 0
+            
+        df1 = pd.DataFrame(finall, columns=['topOrderBuyPerCandle', 'topOrderSellPerCandle'])
+        df= pd.concat([df, df1],  axis = 1)
+        
         print(stkName)
         stored_data = stored_data.iloc[:-1]
         stored_data = pd.concat([stored_data, df], ignore_index=True)
         stored_data['DailyPOCAVG']= stored_data['POC'].cumsum() / (stored_data.index + 1)
         df = stored_data.copy(deep=True)
         df['indes'] = pd.Series([i for i in range(0,len(df))])
+        
 
         fbuyss = []
         fsellss = []
@@ -1511,6 +1534,23 @@ def update_graph_live(n_intervals, sname, stored_data, interval_time, previous_s
         df1 = pd.DataFrame(finall, columns=['topOrderBuy', 'topOrderSell', 'topOrderBuyPercent', 'topOrderSellPercent'])
         df = pd.concat([df, df1],  axis = 1)
         
+        buys = 0
+        sells = 0
+        finall = []
+        for i in range(len(tpo)):
+            for x in tpo[i]:
+                if x[6] == i:
+                    if x[3] == 'A':
+                        sells+=x[1]
+                    elif x[3] == 'B':
+                        buys+=x[1]
+            finall.append([buys,sells])
+            buys = 0
+            sells = 0
+            
+        df1 = pd.DataFrame(finall, columns=['topOrderBuyPerCandle', 'topOrderSellPerCandle'])
+        df= pd.concat([df, df1],  axis = 1)
+        
         print(stkName)
         #df.to_csv(stkName+'Data-3.csv', index=False)
         #prevDf = pd.read_csv(stkName+'Data-2.csv')
@@ -1638,20 +1678,24 @@ def update_graph_live(n_intervals, sname, stored_data, interval_time, previous_s
     #tst = pd.Series([df['topOrderBuy'][i] - df['topOrderSell'][i] for i in  range(len(df))])
 
     df['topDiff'] = df['topOrderBuy'] - df['topOrderSell']
+    
+    df['topDiffPerCandle'] = df['topOrderBuyPerCandle'] - df['topOrderSellPerCandle']
     #df['diff5c'] = df['buyCount5C'] - df['sellCount5C']
     coll = [     'teal' if i > 0
                 else 'crimson' if i < 0
                 else 'gray' for i in df['topDiff']] #tst  df['buySellDif']
 
-    #colll = [     'teal' if i > 0
-    #            else 'crimson' if i < 0
-    #            else 'gray' for i in df['diff5c']]
+    colll = [     'teal' if i > 0
+                else 'crimson' if i < 0
+                else 'gray' for i in df['topDiffPerCandle']]
     
     fig.add_trace(go.Bar(x=pd.Series([i for i in range(len(df))]), y=df['topDiff'], marker_color=coll, name='topOrderDifference'), row=3, col=1) #tst
     #fig.add_trace(go.Bar(x=pd.Series([i for i in range(len(df))]), y=df['diff5c'], marker_color=colll, name='diff5c'), row=2, col=1) #tst
     
-    fig.add_trace(go.Scatter(x=pd.Series([i for i in range(len(df))]), y=df['topOrderBuyPercent'], marker_color='teal', name='topOrderBuyPercent'), row=2, col=1) #tst
-    fig.add_trace(go.Scatter(x=pd.Series([i for i in range(len(df))]), y=df['topOrderSellPercent'], marker_color='crimson', name='topOrderSellPercent'), row=2, col=1)
+    fig.add_trace(go.Bar(x=pd.Series([i for i in range(len(df))]), y=df['topDiffPerCandle'], marker_color=colll, name='topDiffPerCandle'), row=2, col=1) #tst
+
+    #fig.add_trace(go.Scatter(x=pd.Series([i for i in range(len(df))]), y=df['topOrderBuyPercent'], marker_color='teal', name='topOrderBuyPercent'), row=2, col=1) #tst
+    #fig.add_trace(go.Scatter(x=pd.Series([i for i in range(len(df))]), y=df['topOrderSellPercent'], marker_color='crimson', name='topOrderSellPercent'), row=2, col=1)
 
     fig.add_trace(go.Scatter(x=pd.Series([i for i in range(len(df))]), y=df['buyDiffSum'], marker_color='teal', name='buyDiffSum'), row=4, col=1) #tst
     fig.add_trace(go.Scatter(x=pd.Series([i for i in range(len(df))]), y=df['sellDiffSum'], marker_color='crimson', name='sellDiffSum'), row=4, col=1) #tst
@@ -1736,8 +1780,8 @@ def update_graph_live(n_intervals, sname, stored_data, interval_time, previous_s
     # Update y-axis range for the specific subplot
     fig.update_yaxes(
         range=[
-            min([i for i in combined_df['topOrderSellPercent'][int(len(df) * 0.90):len(df)]] + [i for i in combined_df['topOrderBuyPercent'][int(len(df) * 0.90):len(df)]]), 
-            max([i for i in combined_df['topOrderBuyPercent'][int(len(df) * 0.90):len(df)]] + [i for i in combined_df['topOrderSellPercent'][int(len(df) * 0.90):len(df)]])
+            min([i for i in combined_df['topDiffPerCandle'][int(len(df) * 0.90):len(df)]]), 
+            max([i for i in combined_df['topDiffPerCandle'][int(len(df) * 0.90):len(df)]])
         ],
         row=2, col=1
     )
