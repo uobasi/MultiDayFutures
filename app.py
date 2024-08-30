f0ac9eeb1c7a33884a8d3a615ec653963e921b9f
uobasi <54387747+uobasi@users.noreply.github.com>
2024-08-30 16:43:52 -0400

    .

--- app.py
+++ app.py
@@ -1669,8 +1669,8 @@ def update_graph_live(n_intervals, sname, stored_data, interval_time, previous_s
     #fig.add_trace(go.Scatter(x=pd.Series([i for i in range(len(df))]), y=df['topOrderBuyPercent'], marker_color='teal', name='topOrderBuyPercent'), row=2, col=1) #tst
     #fig.add_trace(go.Scatter(x=pd.Series([i for i in range(len(df))]), y=df['topOrderSellPercent'], marker_color='crimson', name='topOrderSellPercent'), row=2, col=1)
 
-    fig.add_trace(go.Bar(x=pd.Series([i for i in range(len(df))]), y=df['buyCount'], marker_color='teal', name='buyCount'), row=3, col=1) #tst
-    fig.add_trace(go.Bar(x=pd.Series([i for i in range(len(df))]), y=df['sellCount'], marker_color='crimson', name='SellCount'), row=3, col=1) #tst
+    fig.add_trace(go.Bar(x=pd.Series([i for i in range(len(df))]), y=df['buyCount'], marker_color='teal', name='topOrderBuyPerCandle'), row=3, col=1) #tst
+    fig.add_trace(go.Bar(x=pd.Series([i for i in range(len(df))]), y=df['topOrderSellPerCandle'], marker_color='crimson', name='topOrderSellPerCandle'), row=3, col=1) #tst
 
     #fig.add_trace(go.Scatter(x=pd.Series([i for i in range(len(df))]), y=df['topOrderBuy'], marker_color='teal'), row=3, col=1) #tst
     #fig.add_trace(go.Scatter(x=pd.Series([i for i in range(len(df))]), y=df['topOrderSell'], marker_color='crimson'), row=3, col=1)
@@ -1760,8 +1760,8 @@ def update_graph_live(n_intervals, sname, stored_data, interval_time, previous_s
 
     fig.update_yaxes(
         range=[
-            min([i for i in combined_df['buyCount'][int(len(df) * 0.90):len(df)]] + [i for i in combined_df['sellCount'][int(len(df) * 0.90):len(df)]]), 
-            max([i for i in combined_df['sellCount'][int(len(df) * 0.90):len(df)]] + [i for i in combined_df['buyCount'][int(len(df) * 0.90):len(df)]])
+            min([i for i in combined_df['topOrderBuyPerCandle'][int(len(df) * 0.90):len(df)]] + [i for i in combined_df['topOrderSellPerCandle'][int(len(df) * 0.90):len(df)]]), 
+            max([i for i in combined_df['topOrderSellPerCandle'][int(len(df) * 0.90):len(df)]] + [i for i in combined_df['topOrderBuyPerCandle'][int(len(df) * 0.90):len(df)]])
         ],
         row=3, col=1
     )
