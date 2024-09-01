a4b80155da3401ef49e7a56c66f46a4ec486261f
uobasi <54387747+uobasi@users.noreply.github.com>
2024-09-01 15:14:56 -0400

    .

--- app.py
+++ app.py
@@ -1647,6 +1647,7 @@ def update_graph_live(n_intervals, sname, stored_data, interval_time, previous_s
     fig.add_trace(go.Scatter(x=pd.Series([i for i in range(len(df))]), y=df['vwapAvg'], mode='lines', opacity=0.5, name='vwapAvg', hovertext=df['time'].tolist() ))
 
 
+
     #tst = pd.Series([df['topOrderBuy'][i] - df['topOrderSell'][i] for i in  range(len(df))])
 
     df['topDiff'] = df['topOrderBuy'] - df['topOrderSell']
