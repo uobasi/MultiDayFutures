c30f02de4a2a18ba2e365109f2c74d2c56f5a381
uobasi <54387747+uobasi@users.noreply.github.com>
2024-09-05 02:20:49 -0400

    .

--- app.py
+++ app.py
@@ -423,7 +423,7 @@ bucket = gclient.get_bucket("stockapp-storage")
 
 from dash import Dash, dcc, html, Input, Output, callback, State
 initial_inter = 550000#60000
-subsequent_inter = 100000 
+subsequent_inter = 90000 
 app = Dash()
 app.layout = html.Div([
     
@@ -1740,7 +1740,7 @@ def update_graph_live(n_intervals, sname, stored_data, interval_time, previous_s
     fig.add_trace(go.Scatter(x=pd.Series([i for i in range(len(df))]), y=df['STDEV_025Cum'], mode='lines', opacity=0.8, name='UPPERVWAP0.25', line=dict(color='black')))
     fig.add_trace(go.Scatter(x=pd.Series([i for i in range(len(df))]), y=df['STDEV_N025Cum'], mode='lines', opacity=0.8, name='LOWERVWAP0.25', line=dict(color='black')))
            
-    fig.add_trace(go.Scatter(x=pd.Series([i for i in range(len(df))]), y=df['prevDayPOC'], mode='lines', name='prevDayPOC', opacity=0.8, line=dict(color='crimson')))
+    fig.add_trace(go.Scatter(x=pd.Series([i for i in range(len(df))]), y=df['prevDayPOC'], mode='lines', name='prevDayPOC', opacity=0.8, line=dict(color='cyan')))
     fig.add_trace(go.Scatter(x=pd.Series([i for i in range(len(df))]), y=df['prevDayHVA'], mode='lines', name='prevDayHVA', opacity=0.8, line=dict(color='green')))   
     fig.add_trace(go.Scatter(x=pd.Series([i for i in range(len(df))]), y=df['prevDayLVA'], mode='lines', name='prevDayLVA', opacity=0.8, line=dict(color='purple'))) 
 
