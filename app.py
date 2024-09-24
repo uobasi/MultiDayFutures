6347126972e5f31237cce49606dd79bc5c8f2186
245 <54387747+uobasi@users.noreply.github.com>
2024-09-24 18:45:45 -0400

    .

--- app.py
+++ app.py
@@ -449,7 +449,7 @@ def calculate_ttm_squeeze(df, n=13):
 #symbolNumList = ['118', '4358', '42012334', '392826', '393','163699', '935', '11232']
 #symbolNameList = ['ES', 'NQ', 'YM','CL', 'GC', 'HG', 'NG', 'RTY']
 
-symbolNumList = ['183748', '106364', '42006053', '230943', '393','163699', '935', '42018437']
+symbolNumList = ['183748', '106364', '42006053', '230943', '393','163699', '923', '42018437']
 symbolNameList = ['ES', 'NQ', 'YM','CL', 'GC', 'HG', 'NG', 'RTY']
 stored_data = None
 
