bdece9a4c5eddf503b53ddd7c30c27afef25614a
245 <54387747+uobasi@users.noreply.github.com>
2025-05-07 19:58:00 -0400

    .

--- app.py
+++ app.py
@@ -689,6 +689,16 @@ def update_graph_live(n_intervals, relayout_data, sname, interv, stored_data, pr
                 
     df = pd.concat([prevDf, df_resampled2], ignore_index=True)
     
+    df['topDiffNega'] = ((df['topBuys'] - df['topSells']).apply(lambda x: x if x < 0 else np.nan)).abs()
+    df['topDiffPost'] = (df['topBuys'] - df['topSells']).apply(lambda x: x if x > 0 else np.nan)
+    
+    df['percentile_topBuys'] =  [percentileofscore(df['topBuys'][:i+1], df['topBuys'][i], kind='mean') for i in range(len(df))]
+    df['percentile_topSells'] = [percentileofscore(df['topSells'][:i+1], df['topSells'][i], kind='mean') for i in range(len(df))] 
+
+    df['percentile_Posdiff'] =  [percentileofscore(df['topDiffPost'][:i+1].dropna(), df['topDiffPost'][i], kind='mean') if not np.isnan(df['topDiffPost'][i]) else None for i in range(len(df))]
+    df['percentile_Negdiff'] =  [percentileofscore(df['topDiffNega'][:i+1].dropna(), df['topDiffNega'][i], kind='mean') if not np.isnan(df['topDiffNega'][i]) else None for i in range(len(df))]
+
+    
     
     df['smoothed_1ema'] = butter_lowpass_realtime(df["close"],cutoff=0.5, order=2)
     df['POCDistanceEMA'] = (df['smoothed_1ema'] - df['allPOC']) / df['allPOC'] * 100
